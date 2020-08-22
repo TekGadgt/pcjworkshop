d431a306027708c5e388c21f7d4468c2364f25a6
Ryan McGovern <tekgadgt@devportfol.io>
2020-08-22 15:07:11 -0400

    formatting and added home link to logo

--- src/http/get-echo-000word/index.js
+++ src/http/get-echo-000word/index.js
@@ -1,11 +1,11 @@
-exports.handler = async function http(req){
+exports.handler = async function http(req) {
   let { word } = req.pathParameters
   console.log(req)
   return {
     statusCode: 200,
     headers: {
-      'content-type':'application/json; charset=utf-8'
+      'content-type': 'application/json; charset=utf-8',
     },
-    body: JSON.stringify({echo: word})
+    body: JSON.stringify({ echo: word }),
   }
 }
--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -9,28 +9,31 @@ let body = `
 		<link rel="stylesheet" href="_static/default.css">
 	</head>
 	<body>
-    <h1>Hello Beginners!</h1>
+    <h1><a>Hello Beginners!</a></h1>
     <img src="_static/me_begin.jpg">
 		<p>Oh yea, wait a minute</p>
-     <form action="/name" method="post">
+    <form action="/name" method="post">
       <label>First Name :</label>
       <input type="text" name="first" value="Mr."><br>
       <label>Last Name :</label>
       <input type="text" name="last" value="POSTman"><br><br>
       <input type="submit" value="Submit">
     </form>
+    <script>
+      document.querySelector('a').href = document.location.origin
+    </script>
 	</body>
 </html>
 `
 // boilerplate Lambda function handler
 exports.handler = async function http(req) {
-  console.log('Praise Cage')
+  // console.log('Praise Cage')
   return {
     statusCode: 200,
     headers: {
       'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
     },
-    body
+    body,
   }
 }
